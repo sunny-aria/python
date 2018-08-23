803d28e2030ab6d3755da39e40d6d3c969d3be62
dongfeng <sundongfeng@yazuo.com>
2018-08-23 17:05:32 +0800

    py hmac加盐md5加密

---
+++ hmacDemo.py
@@ -0,0 +1,30 @@
+import hmac,random
+
+def hmac_md5(key,s):
+    # h = hmac.new(key, message, digestmod='MD5')
+    # 如果消息很长，可以多次调用h.update(msg)
+    return hmac.new(key.encode('utf-8'),s.encode('utf-8'),'MD5').hexdigest()
+
+class User(object):
+    def __init__(self,username,password):
+        self.username = username
+        self.key=''.join([chr(random.randint(48,122)) for i in range(20)])
+        self.password = hmac_md5(self.key,password)
+
+db={
+    'michael':User('michael','123456'),'bob':User('bob','abc999'),'alice':User('alice','alice2018')
+}
+
+def login(username,password):
+    user = db[username]
+    return user.password == hmac_md5(user.key,password)
+
+
+# 测试:
+assert login('michael', '123456')
+assert login('bob', 'abc999')
+assert login('alice', 'alice2018')
+assert not login('michael', '1234567')
+assert not login('bob', '123456')
+assert not login('alice', 'Alice2008')
+print('ok')
