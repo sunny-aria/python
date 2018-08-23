860e020f8f88add2e842ba1f628035a907a9ee80
dongfeng <sundongfeng@yazuo.com>
2018-08-23 15:06:46 +0800

    py haslib md5 sha1 测试，加盐

---
+++ md5SaltDemo.py
@@ -0,0 +1,33 @@
+import hashlib,random
+'''
+要确保存储的用户口令不是那些已经被计算出来的常用口令的MD5，这一方法通过对原始口令加一个复杂字符串来实现，俗称“加盐”：
+'''
+
+def get_md5(s):
+    return hashlib.md5(s.encode('utf-8')).hexdigest()
+
+class User(object):
+    def __init__(self, username, password):
+        self.username = username
+        self.salt = ''.join([chr(random.randint(48, 122)) for i in range(20)]) # x for x in foo if x%3==0 注意这种简写方法
+        self.password = get_md5(password + self.salt)
+
+db = {
+    'michael': User('michael', '123456'),
+    'bob': User('bob', 'abc999'),
+    'alice': User('alice', 'alice2008')
+}
+
+def login(username, password):
+    user = db[username]
+    password = password+user.salt # user.salt 获取到用户的盐
+    return user.password == get_md5(password)
+
+# 测试:
+assert login('michael', '123456')
+assert login('bob', 'abc999')
+assert login('alice', 'alice2008')
+assert not login('michael', '1234567')
+assert not login('bob', '123456')
+assert not login('alice', 'Alice2008')
+print('ok')
