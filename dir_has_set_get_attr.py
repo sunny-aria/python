1b8cb17e851591707ec3565082b9c7d4c08be37f
dongfeng <sundongfeng@yazuo.com>
2018-08-28 10:21:14 +0800

    py

--- dir_has_set_get_attr.py
+++ dir_has_set_get_attr.py
@@ -1,19 +1,19 @@
 type()
 isinstance(c,object)
->>>>class A(object):
+class A(object):
     def __init__(self):
         self.x=10
 
 
->>>>a =A()
->>>>dir(a)
-dir() 列出
->>>>hasattr(a,'x')
->>>>setattr(a,'x',30)
->>>>getattr(a,'x')
+a =A()
+dir(a)
+# dir() 列出
+hasattr(a,'x')
+setattr(a,'x',30)
+getattr(a,'x')
 
-实例属性
-类属性，相当于java 的static 属性，属于类，所有实例对象共用
+# 实例属性
+# 类属性，相当于java 的static 属性，属于类，所有实例对象共用
 class Student(object):
     count = 0
 
